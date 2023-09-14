61f0f2779d255af32ad58d06dd319a5599440468
seandavidreed <seanreed7992@gmail.com>
2023-09-13 21:02:57 -0700

    Initial push to experimental

--- spectrogram.py
+++ spectrogram.py
@@ -1,9 +1,21 @@
 from scipy.io import wavfile
 import matplotlib.pyplot as plt
+import sys
 import os
 
 def main():
-    file_list = os.scandir('hammerons')
+    if len(sys.argv) != 4:
+        print("Usage: spectrogram /source_dir /target_dir <filename>")
+        return 1
+    if not os.path.isdir(sys.argv[1]) or not os.path.isdir(sys.argv[2]):
+        print("Arguments supplied must be: DIR DIR STR")
+        return 1
+
+    source = sys.argv[1]
+    target = sys.argv[2]
+    specname = sys.argv[3]
+
+    file_list = os.scandir(source)
     print(file_list)
     for i, filename in enumerate(file_list):
         plt.rcParams['figure.figsize'] = [3.0, 3.0]
@@ -12,7 +24,7 @@ def main():
         aud = aud[:,0]
         plt.specgram(aud, Fs=Fs)
         plt.axis(False)
-        plt.savefig(f'images/true/true-classical-{i}.png')
+        plt.savefig(f'{target}/{specname}-{i}.png')
 
 if __name__ == "__main__":
     main()
